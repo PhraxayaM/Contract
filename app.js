ac906b911d694c6deb0b70f05b7b3a5c9f77d64b
MattHew Phraxayavong <mphraxa@gmail.com>
2019-12-02 16:34:55 -0800

    update

--- app.js
+++ app.js
@@ -35,9 +35,9 @@ donationsController(app);
 
 
 //review.js
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
-// app.set( 'port', ( process.env.PORT || 5000 ));
+app.set( 'port', ( process.env.PORT || 5000 ));
 
 
 
